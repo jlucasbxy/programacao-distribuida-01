ebf4225501f6b5f2a78ff9d8711b231e85d190c3
jlucasbx <jlucasb16@gmail.com>
2026-04-15 10:56:54 -0300

    refactor: remove client class and --client mode from data-server

--- data-server/src/main/java/com/example/dataserver/Main.java
+++ data-server/src/main/java/com/example/dataserver/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
             System.err.println("Usage: Main --server [port] [dataFilePath]");
-            System.err.println("       Main --client [host] [port]");
             System.exit(1);
         }
 
@@ -16,10 +15,9 @@ public class Main {
 
         switch (mode) {
             case "--server" -> new DataServer(ServerConfig.fromArgs(rest)).start();
-            case "--client" -> new DataServerClient(rest).start();
             default -> {
                 System.err.println("Unknown mode: " + mode);
-                System.err.println("Use --server or --client.");
+                System.err.println("Use --server.");
                 System.exit(1);
             }
         }
